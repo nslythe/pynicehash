ef33eb6854aa0996b7545272d3b27966cfa1747e
Nicolas Slythe <nicolas@slythe.net>
2024-11-08 10:09:59 -0500

    update rig

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pynicehash",
-    version="0.0.5",
+    version="0.0.10",
     author="Nicolas Slythe",
     author_email="pynicehash@slythe.net",
     description="Python to interac with Nicehash API v2",
--- src/pynicehash/miningrig.py
+++ src/pynicehash/miningrig.py
@@ -6,27 +6,40 @@ class MiningRig(object):
         self.__set_data(data)
     
     def __set_data(self, data):
+        self.hasV4Rigs = data.get("hasV4Rigs", False)
+        self.is_managed = data.get("type", "UNMANAGED") == "MANAGED"
         self.id = data["rigId"]
         self.type = data["type"]
         self.name = "unnamed"
         if "name" in data:
             self.name = data["name"]
         else:
-            if "v4" in data:
+            if self.hasV4Rigs:
                 if "mmv" in data["v4"]:
                     if "workerName" in data["v4"]["mmv"]:
                         self.name = data["v4"]["mmv"]["workerName"]
 
         self.status_time = data["statusTime"]
         self.miner_status = data["minerStatus"]
-        self.group_name = data["groupName"]
-        self.unpaid_amount = data["unpaidAmount"]
-        self.profitability = data["profitability"]
-        self.local_profitability = data["localProfitability"]
+        if "groupName" in data:
+            self.group_name = data["groupName"]
+        if "unpaidAmount" in data:
+            self.unpaid_amount = data["unpaidAmount"]
+        if "profitability" in data:
+            self.profitability = data["profitability"]
+        if "localProfitability" in data:
+            self.local_profitability = data["localProfitability"]
         
-        self.devices: MiningDevice = []
-        for d in data["v4"]["devices"]:
-            self.devices.append(MiningDevice(self.nicehash_api, self, d))
+        if data["type"] == "MANAGED":
+            self.devices: MiningDevice = []
+            device_list = None
+            if self.hasV4Rigs:
+                device_list = data["v4"]["devices"]
+            else:
+                device_list = data["devices"]
+
+            for d in device_list:
+                self.devices.append(MiningDevice(self.nicehash_api, self, d))
 
     def update(self):
         self.__set_data(self.nicehash_api.get_rig_detail(self.id))
